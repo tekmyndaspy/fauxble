22242dfa91e126833f686fab89a023b75c2999f9
sadie k. <96924865+tekmyndaspy@users.noreply.github.com>
2024-12-05 17:40:27 -0500

    Update fauxble.cmd
    add more feedback, move used directories into variables

--- fauxble.cmd
+++ fauxble.cmd
@@ -4,12 +4,15 @@
 @echo off
 
 :: set videoplayer and commandline flags to play video
+:: set directories to pull videos from
 :: videoplayer variable takes the command to run the videoplayer and flags to pass to the videoplayer and nothing else
 :: filetypes variable accepts a list of filetypes delineated by spaces and using stars in place of the potential filename
 set videoplayer=mpv --fs
 set filetypes=*.mp4
+set maindirectory=Main
+set intermediarydirectory=Intermediary
 
-:: evaluate !! variable evluations
+:: evaluate !! variable evaluations
 setlocal enableDelayedExpansion
 
 :: set window title to 'fauxble'
@@ -24,7 +27,7 @@ echo Starting Fauxble.
 echo Choosing show.
 
 :: move to shows directory in same folder as batch file
-cd /d "%~dp0\Shows"
+cd /d "%~dp0\%maindirectory%"
 
 :: count total folders in working directory and store into array
 set /a "i=0"
@@ -33,11 +36,13 @@ for /d %%d in (*) do (
 	set "directorylist[!i!]=%%~d"
 )
 
+echo Moving to !directorylist[%rnd%]!.
+
 :: move to random folder in shows folder
 set /a rnd="%random% %%%i% +1"
 cd "!directorylist[%rnd%]!"
 
-echo Playing episode.
+echo Choosing episode.
 
 :: count total of chosen filetypes and store into array
 set /a "i=0"
@@ -48,15 +53,20 @@ for %%f in (%filetypes%) do (
 
 :: get random number between 1 and array size
 set /a rnd="%random% %%%i% +1"
+
+echo Playing !videolist[%rnd%]!.
 
 :: play random episode
 start /wait %videoplayer% "!videolist[%rnd%]!"
 
+:: remove selected video from array
+set videolist[%rnd%]=ERRORNOVIDEOCHOSEN
 
 
-echo Playing bumper.
 
-cd /d "%~dp0\Bumpers"
+echo Choosing bumper.
+
+cd /d "%~dp0\%intermediarydirectory%"
 
 :: count total of chosen filetypes and store into array
 set /a "i=0"
@@ -68,8 +78,13 @@ for %%f in (%filetypes%) do (
 :: get random number between 1 and array size
 set /a rnd="%random% %%%i% +1"
 
+echo Playing !videolist[%rnd%]!.
+
 :: play random bumper
 start /wait %videoplayer% "!videolist[%rnd%]!"
+
+:: remove selected video from array
+set videolist[%rnd%]=ERRORNOVIDEOCHOSEN
 
 timeout /t 10
 
